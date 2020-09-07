a706154448b5fe5c8ace61417adac3fc03922640
Anis <anisberchem1189@gmail.com>
2020-09-07 10:32:34 -0700

    Test en js

---
+++ public/js/index.js
@@ -0,0 +1,130 @@
+// #2
+
+// var maBoolean = true;
+
+//#3
+// let monArray = [];
+
+//#4
+
+// let Anis = {
+// 
+
+// }
+
+//#5
+// let string = "CodingWeek";
+
+//#6
+// let monInt = ...
+
+// #7
+
+// let monObjet = {
+//     nom: "Anis",
+//      age: 22,
+// }
+
+// #8 
+// let anis = (nom,age) => {
+
+// }
+
+// #9
+// let monInt = 5
+
+// if (monInt < 8) {
+//     console.log(` Le nombre ${monInt} est plus petit que 8`);
+    
+    
+// } else {
+//     console.log(` Le nombre ${monInt} est plus grand que 8`);
+    
+// }
+
+// #10
+
+// for (let i = 0; i < 10; i++) {
+//    console.log(i);
+   
+    
+// }
+
+// #11
+
+// var date = 
+
+// #12
+
+// function majuscule(mot) {
+//     console.log(mot.toUpperCase());
+    
+// }
+// majuscule("anis")
+
+// #13
+// function majuscule(mot) {
+//     console.log(mot.substr(0,1).toUpperCase(), mot.substr(3,1).toUpperCase());
+    
+// }
+// majuscule("anis")
+
+// #14
+
+// function longeur(mot) {
+//     console.log(mot.length);
+    
+    
+// }
+// longeur("kdjfioeiojrio")
+
+// #15
+
+// function chiffre(number) {
+//     alert(Math.random() * 100)
+// }
+// chiffre()
+
+// #16
+
+// function chiffre(chiffre1,chiffre2) {
+//     console.log();
+    
+// }
+
+// #18
+
+// let objet = {
+//     nom:
+//     age: 
+   
+// }
+
+
+// #20
+// let carre = {
+//     ajout(taille,bordure,epaisseur,couleur) {
+//         this.size = taille;
+//         this.borderaspect = bordure;
+//         this.epaisse = epaisseur;
+//         this.coul = couleur;
+//     }    
+// }
+
+// if (this.borderaspect !== "solid" || "dotted") {
+//     alert("different")
+
+    
+// } else if (this.coul !== "red" || "blue" || "green") {
+//     alert("Faux")
+// } else if (this.epaisse && this.size ) {
+    
+// }
+
+
+
+
+
+
+
+
